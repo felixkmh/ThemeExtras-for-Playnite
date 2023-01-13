289032671b30e41b553b34713ad181eb927592a0
felixkmh <24227002+felixkmh@users.noreply.github.com>
2023-01-13 19:50:51 +0100

    prepared combined rating

---
+++ source/Controls/CombinedRating.xaml.cs
@@ -0,0 +1,36 @@
+﻿using Playnite.SDK.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Extras.Controls
+{
+    /// <summary>
+    /// Interaktionslogik für UserRating.xaml
+    /// </summary>
+    public partial class CombinedRating : Playnite.SDK.Controls.PluginUserControl
+    {
+        public CombinedRating()
+        {
+            InitializeComponent();
+        }
+
+        public override void GameContextChanged(Game oldContext, Game newContext)
+        {
+            base.GameContextChanged(oldContext, newContext);
+            RatingsBar.DataContext = newContext;
+        }
+    }
+}
